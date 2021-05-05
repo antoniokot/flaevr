609dc16df425ac78504585113030ecb9559d88ec
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-05-05 12:21:11 -0300

    Signup and Login Pages

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
