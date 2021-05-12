699cd48e32832a1e7277ba2afa8a40b38e2b6d1f
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-05-12 10:54:33 -0300

    Create button.dart

---
+++ lib/components/button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  @override
+  Button(
+      {@required this.padding,
+      @required this.constraints,
+      @required this.width,
+      @required this.height,
+      @required this.text,
+      @required this.onPressed,
+      @required this.textStyle,
+      @required this.backgroundColor});
+
+  final EdgeInsets padding;
+  final BoxConstraints constraints;
+  final double width;
+  final double height;
+  final String text;
+  final Function onPressed;
+  final TextStyle textStyle;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: padding,
+      constraints: constraints,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: backgroundColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              )),
+          child: Text(text, style: textStyle),
+          onPressed: () => onPressed(),
+        ),
+      ),
+    );
+  }
+}
