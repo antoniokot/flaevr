08d99d532d3bb3cf194bf7c642f965037067a961
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-08-04 11:49:22 -0300

    New Menu and Profile Layout

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,24 +10,100 @@ class Profile extends StatefulWidget {
 }
 
 class ProfileState extends State<Profile> {
+  bool isVisible = true;
+  bool isOpaque = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: Button(
-        padding: EdgeInsets.only(left: 40, right: 40, bottom: 11, top: 20),
-        constraints: BoxConstraints(minWidth: 100, maxWidth: 300),
-        width: double.infinity,
-        height: 42,
-        text: "Registrar-se com o Email",
-        textStyle: TextStyle(
-            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
-        backgroundColor: Color.fromRGBO(255, 70, 70, 1),
-        onPressed: () => {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => Config()))
-        },
-      ),
-    ));
+    final Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Stack(
+            children: <Widget>[
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 60,
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 15,
+                        itemBuilder: (BuildContext context, int index) => Card(
+                          child: Center(child: Text('Dummy Card Text')),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Favoritos',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Data de Modificação',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Icon(Icons.expand_more)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height - 200,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        itemCount: 15,
+                        itemBuilder: (BuildContext context, int index) => Card(
+                          child: Center(child: Text('Dummy Card Text')),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                    alignment: Alignment.center,
+                    height: 50,
+                    width: size.width,
+                    color: Colors.blue,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          'https://images.pexels.com/photos/4345992/pexels-photo-4345992.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                                      fit: BoxFit.fill),
+                                ),
+                              ),
+                              Text('Mariana Melo'),
+                              Icon(Icons.settings)
+                            ]))),
+              ),
+            ],
+          )),
+    );
   }
 }
--- lib/pages/spa.dart
+++ lib/pages/spa.dart
@@ -28,28 +28,92 @@ class OriginState extends State<Origin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Padding(
-          padding: EdgeInsets.only(top: 20),
-          child: SizedBox(
-            height: 60,
-            width: 60,
-            child: FloatingActionButton(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              onPressed: () {},
-              child: Container(
-                height: 70,
-                width: 70,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, color: Color(0xFFFF4646)),
-                child: Icon(Icons.camera_alt_outlined, size: 32),
-              ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(top: 20, bottom: 15),
+        child: SizedBox(
+          height: 60,
+          width: 60,
+          child: FloatingActionButton(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            onPressed: () {},
+            child: Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xFFFF4646)),
+              child: Icon(Icons.camera_alt_outlined, size: 32),
             ),
           ),
         ),
-        body: _children[_currentIndex],
-        bottomNavigationBar: Theme(
+      ),
+      body: Stack(children: [
+        _children[_currentIndex],
+        Positioned(
+            left: 30.0,
+            right: 30.0,
+            bottom: 20.0,
+            child: new Container(
+                width: MediaQuery.of(context).size.width - 60,
+                height: 50.0,
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.home,
+                          color: Color(0xFF3d3d4e),
+                        ),
+                        onPressed: () {
+                          onTabTapped(0);
+                        }),
+                    IconButton(
+                        icon: Icon(
+                          Icons.search_outlined,
+                          color: Color(0xFF3d3d4e),
+                        ),
+                        onPressed: () {
+                          onTabTapped(1);
+                        }),
+                    IconButton(
+                        icon: Icon(
+                          Icons.home,
+                          color: Color(0xFF3d3d4e),
+                        ),
+                        onPressed: () {}),
+                    IconButton(
+                        icon: Icon(
+                          Icons.favorite_border,
+                          color: Color(0xFF3d3d4e),
+                        ),
+                        onPressed: () {
+                          onTabTapped(3);
+                        }),
+                    IconButton(
+                        icon: Icon(
+                          Icons.person_outline,
+                          color: Color(0xFF3d3d4e),
+                        ),
+                        onPressed: () {
+                          onTabTapped(4);
+                        }),
+                  ],
+                )))
+      ]),
+      /*bottomNavigationBar: Theme(
           data: Theme.of(context).copyWith(
             // sets the background color of the `BottomNavigationBar`
             canvasColor: Colors.white,
@@ -100,6 +164,7 @@ class OriginState extends State<Origin> {
                   ))
             ],
           ),
-        ));
+        )*/
+    );
   }
 }
