b6e129459728f51b68950b4dae56e3ff8bff1058
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-09-08 10:42:54 -0300

    Adjustments

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
-import 'package:flaevr/pages/config.dart';
 import 'package:flutter/material.dart';
-import 'package:flaevr/components/button.dart';
+import 'package:flaevr/models/user.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -12,29 +14,48 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
   String nome = "Mariana";
 
+  void initState() {
+    super.initState();
+    fetchById().then((user) => {nome = user.name});
+  }
+
+  Future<User> fetchById() async {
+    final id = 2;
+    final response =
+        await http.get(Uri.parse('/users/unique/' + id.toString()));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load user');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     //final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white, 
-        body: Padding(
-          padding: const EdgeInsets.all(29.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Olá, " + nome,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Color(0xFF3D3D4E)),
-              ),
-              
-            ],
-          ),
-        )
-      ),
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.all(29.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Olá, " + nome,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Color(0xFF3D3D4E)),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
