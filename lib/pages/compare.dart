5989858b9658b1633b31bc0204ec03625bfe7d9b
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-09-30 10:36:42 -0300

    Create compare.dart

---
+++ lib/pages/compare.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Compare extends StatefulWidget {
+  Compare({Key key}) : super(key: key);
+
+  @override
+  CompareState createState() => CompareState();
+}
+
+class CompareState extends State<Compare> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
