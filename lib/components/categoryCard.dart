3143b3dd2d0f3a123e44a175fcd5010e6394bda8
Eduardo Migueis <edmigueis@hotmail.com>
2021-11-05 11:28:31 -0300

    Home Page

---
+++ lib/components/categoryCard.dart
@@ -0,0 +1,75 @@
+import 'package:flaevr/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  @override
+  CategoryCard(this.icon, this.title, this.stats);
+
+  final ImageProvider icon;
+  final double stats;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 130,
+        decoration: new BoxDecoration(
+          color: Styles.ultraLightMutedGrey,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.23),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 2), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: Styles.sidePaddingWithVerticalSpace,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                this.title,
+                style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF3d3d4e)),
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Center(
+                child: Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: icon, fit: BoxFit.fill),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                this.stats.toString() + "%",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF3d3d4e)),
+              ),
+              Text(
+                "dos produtos escaneados.",
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Styles.mutedGrey),
+              )
+            ],
+          ),
+        ));
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,12 @@
+import 'package:flaevr/components/categoryCard.dart';
 import 'package:flaevr/components/gaugeChart.dart';
 import 'package:flaevr/components/ripple.dart';
-import 'package:flaevr/components/skeleton.dart';
 import 'package:flaevr/models/User.dart';
-import 'package:flaevr/services/UserService.dart';
 import 'package:flaevr/utils/styles.dart';
+import 'package:flaevr/utils/utility.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:flaevr/utils/sessionManager.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -25,6 +25,32 @@ class HomeState extends State<Home> {
     });
   }
 
+  String getFormattedDate() {
+    DateTime date = DateTime.now();
+    String day = DateFormat('dd').format(date);
+    var months = [
+      "JAN",
+      "FEV",
+      "MAR",
+      "ABR",
+      "MAI",
+      "JUN",
+      "JUL",
+      "AGO",
+      "SET",
+      "OUT",
+      "NOV",
+      "DEZ"
+    ];
+    String month = months[int.parse(DateFormat('MM').format(date))];
+    String dayOfTheWeek = DateFormat('EEEE').format(date);
+    return (Utility.capitalize(dayOfTheWeek.substring(0, 3)) +
+        ", " +
+        day +
+        " " +
+        month);
+  }
+
   void initState() {
     super.initState();
     getUser();
@@ -41,52 +67,141 @@ class HomeState extends State<Home> {
                   const EdgeInsets.symmetric(horizontal: 19.0, vertical: 29.0),
               child: Column(
                 children: <Widget>[
-                  Text(
-                    "Olá, " +
-                        (this.user == null
-                            ? ""
-                            : this.user!.name!.split(" ")[0]) +
-                        ", você já escaneou 5 itens essa semana.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                        color: Color(0xFF3D3D4E)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              getFormattedDate(),
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 13,
+                                  color: Color(0xFFFF4646)),
+                            ),
+                            Text(
+                              "Olá, " +
+                                  (this.user == null
+                                      ? ""
+                                      : this.user!.name!.split(" ")[0]),
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: Color(0xFF3D3D4E)),
+                            ),
+                          ]),
+                      Container(
+                        width: 55,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: this.user == null
+                                  ? AssetImage(
+                                      "lib/assets/images/avatars/prof1.png")
+                                  : Utility.isNumeric(this.user!.avatar) ==
+                                          false
+                                      ? NetworkImage(this.user!.avatar!)
+                                      : AssetImage(
+                                          "lib/assets/images/avatars/prof" +
+                                              this.user!.avatar! +
+                                              ".png") as ImageProvider,
+                              fit: BoxFit.fill),
+                        ),
+                      ),
+                    ],
                   ),
                   Container(
                       width: MediaQuery.of(context).size.width,
                       height: 170,
                       child: Center(
-                        child: RipplesAnimation(
-                          animateChild: false,
-                          size: 40,
-                          color: Color(0xFFFF4646),
-                          child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(75)),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.23),
-                                    spreadRadius: 2,
-                                    blurRadius: 3,
-                                    offset: Offset(
-                                        0, 2), // changes position of shadow
-                                  ),
-                                ],
-                              ),
-                              width: 75,
-                              height: 75,
-                              child: Center(
-                                  child: Text(
-                                "5",
-                                style: TextStyle(
-                                    fontSize: 27.0,
-                                    color: Styles.textBlack,
-                                    fontWeight: FontWeight.bold),
-                              ))),
-                        ),
+                        child: Container(
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Styles.ultraLightMutedGrey,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.23),
+                                  spreadRadius: 2,
+                                  blurRadius: 3,
+                                  offset: Offset(
+                                      0, 2), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 30),
+                                child: Row(
+                                  children: [
+                                    RipplesAnimation(
+                                      animateChild: false,
+                                      size: 25,
+                                      color: Color(0xFFFF4646),
+                                      child: Container(
+                                          decoration: BoxDecoration(
+                                            color: Color(0xFFFF4646),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(75)),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.grey
+                                                    .withOpacity(0.23),
+                                                spreadRadius: 2,
+                                                blurRadius: 3,
+                                                offset: Offset(0,
+                                                    2), // changes position of shadow
+                                              ),
+                                            ],
+                                          ),
+                                          width: 45,
+                                          height: 45,
+                                          child: Center(
+                                              child: Text(
+                                            "5",
+                                            style: TextStyle(
+                                                fontSize: 27.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          ))),
+                                    ),
+                                    Flexible(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 12),
+                                              child: Text("Seu score",
+                                                  style: TextStyle(
+                                                      fontSize: 16.0,
+                                                      color: Styles.textBlack,
+                                                      fontWeight:
+                                                          FontWeight.w600))),
+                                          SizedBox(
+                                            height: 4,
+                                          ),
+                                          Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 12),
+                                              child: Text(
+                                                  "Você escaneou 21 produtos essa semana!",
+                                                  style: TextStyle(
+                                                      fontSize: 13.0,
+                                                      color: Styles.mutedGrey,
+                                                      fontWeight:
+                                                          FontWeight.w500)))
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ))),
                       )),
                   Center(
                       child: Container(
@@ -170,27 +285,38 @@ class HomeState extends State<Home> {
               builder:
                   (BuildContext context, ScrollController scrollController) {
                 return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(20)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.23),
-                        spreadRadius: 2,
-                        blurRadius: 3,
-                        offset: Offset(0, -1), // changes position of shadow
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.23),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: Offset(0, -1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: Styles.sidePaddingWithVerticalSpace,
+                      child: GridView.builder(
+                        physics: BouncingScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            mainAxisSpacing: 30,
+                            crossAxisSpacing: 30,
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.9),
+                        controller: scrollController,
+                        itemCount: 25,
+                        itemBuilder: (BuildContext context, int index) {
+                          return CategoryCard(
+                              AssetImage("lib/assets/images/badges/Vegano.png"),
+                              "Veganos",
+                              24);
+                        },
                       ),
-                    ],
-                  ),
-                  child: ListView.builder(
-                    controller: scrollController,
-                    itemCount: 25,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(title: Text('Item $index'));
-                    },
-                  ),
-                );
+                    ));
               },
             ),
           ])),
