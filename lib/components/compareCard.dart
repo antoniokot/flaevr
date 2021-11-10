73dad3af3e9b7ff553b8a8fb1c1c65adc5d7d6b4
Matheus Seiji <Matheus.noda@gmail.com>
2021-11-10 13:47:18 -0300

    finish completing functional mech on compare

--- lib/components/compareCard.dart
+++ lib/components/compareCard.dart
@@ -1,65 +1,140 @@
-import 'package:flaevr/pages/results.dart';
+import 'package:flaevr/models/ProductModel.dart';
+import 'package:flaevr/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flaevr/utils/colorGenerator.dart';
+import 'dart:async';
 
 class CompareCard extends StatefulWidget {
-  const CompareCard({Key? key}) : super(key: key);
-
   @override
-  _CompareCardState createState() => _CompareCardState();
+  CompareCard(
+      {required this.heightAspectRatio,
+      required this.width,
+      required this.product})
+      : assert(heightAspectRatio.aspectRatio > 0 && width > 0);
+
+  final AspectRatio heightAspectRatio;
+  final double width;
+  final ProductModel product;
+  @override
+  CompareCardState createState() => CompareCardState();
 }
 
-class _CompareCardState extends State<CompareCard> {
+class CompareCardState extends State<CompareCard> {
+  var _mainColor;
+
+  @override
+  void initState() {
+    super.initState();
+    getMainColors(
+        new NetworkImage(this.widget.product.pictureUrl != null
+            ? this.widget.product.pictureUrl.toString()
+            : "https://media.istockphoto.com/photos/doing-business-with-a-smile-picture-id1330547068?s=612x612"),
+        new Size(500, 500));
+  }
+
+  Future<void> getMainColors(ImageProvider img, Size size) async {
+    await ColorGenerator.getMainColors(img, size, 1).then((value) => {
+          _mainColor = ColorGenerator.getColorByImportance(value)?.color,
+          if (mounted) setState(() {})
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GestureDetector(
-        child: IntrinsicWidth(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 180, // só aceita q eh esse numero
-                width: 400,
-                decoration: BoxDecoration(
-                  //color: Color(0xFFFFFFFF),
-                  color: Color(0xFFFE6767),
+      height: (widget.heightAspectRatio.aspectRatio * widget.width),
+      width: widget.width,
+      margin: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.23),
+            spreadRadius: 2,
+            blurRadius: 3,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
                   borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(18.0),
-                      topLeft: Radius.circular(18.0),
-                      bottomRight: Radius.circular(18.0),
-                      bottomLeft: Radius.circular(18.0)),
-                ),
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16)),
+                  color: _mainColor),
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16)),
+                child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Image.network(
+                      this.widget.product.pictureUrl != null
+                          ? this.widget.product.pictureUrl as String
+                          : "https://media.istockphoto.com/photos/doing-business-with-a-smile-picture-id1330547068?s=612x612",
+                      fit: BoxFit.contain,
+                    )),
+              ),
+            ),
+          ),
+          Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16)),
+                  color: Colors.white
+              ),
+              //width: double.infinity,
+              //height: 65,
+              width: 175,
+              height: 125,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.tag_faces,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                    Text(
-                      "Parece que você ainda não adicionou nenhum produto para comparar. Quer adicionar um?",
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Text(
+                        this.widget.product.name!.split(',')[0],
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Color(0xff3d3d4e),
+                            fontWeight: FontWeight.w500),
                       ),
                     ),
-                    Text(
-                      "Adicionar",
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        this.widget.product.name!.split(',')[1],
+                        style: TextStyle(
+                            fontSize: 11,
+                            color: Styles.mutedGrey,
+                            fontWeight: FontWeight.normal),
                       ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(""),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Color(0xff3d3d4e),
+                          size: 16,
+                        )
+                      ],
+                    )
                   ],
                 ),
-              ),
-            ],
-          ),
-        ),
-        onTap: () => {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => Results()))
-        },
+              ))
+        ],
       ),
     );
   }
